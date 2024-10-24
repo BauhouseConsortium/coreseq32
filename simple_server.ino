1eac586b6be8e45dbf677739f56db73af062f806
Budi Prakosa <budi.prakosa@gmail.com>
2024-10-24 07:51:47 +0700

    chore license

--- simple_server.ino
+++ simple_server.ino
@@ -1,8 +1,33 @@
 //
-// A simple server implementation showing how to:
-//  * serve static messages
-//  * read GET and POST parameters
-//  * handle missing pages / 404s
+// C0R3SEQ32: A Sequencer Server for ESP32
+//
+// This program implements a web server on ESP32 that:
+//  * Controls 8 relays in sequenced patterns
+//  * Serves a web interface for pattern editing
+//  * Handles API requests for pattern manipulation
+//  * Stores patterns in SPIFFS for persistence
+//
+// Author: Budi Prakosa (aka manticore)
+// License: MIT License
+// Copyright (c) 2024 Budi Prakosa
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 //
 
 #include <Arduino.h>
